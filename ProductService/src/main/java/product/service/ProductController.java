5965e09d4e0d30c0553c22808ee8e7a511ce864c
George COVRIG <covrig.george.manuel@gmail.com>
2023-12-29 00:40:02 +0200

    example security keycloak

---
+++ ProductService/src/main/java/product/service/ProductController.java
@@ -0,0 +1,24 @@
+package product.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    List<String> products = List.of("Beans", "Oat cereals", "Bread", "Milk");
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<String>> getProducts() {
+        return ResponseEntity.ok(products);
+
+    }
+
+}
